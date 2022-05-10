a6951f445b967c0379b929c37541af060dfef11c
fckkmoon <maria.brunhara@etec.sp.gov.br>
2022-05-10 12:27:28 -0300

    atualização codigo

---
+++ Assets/raposa.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public vector3 
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Input.GetKey(KeyCode.w)
+        Input.GetAxis(x)
+    }
+}
